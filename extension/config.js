74a3066f5274315a065f02aa9504acb4d5fdbafa
retz8 <ekkicb71@gmail.com>
2026-01-02 20:16:00 +0900

    .

--- extension/config.js
+++ extension/config.js
@@ -1,7 +1,7 @@
 // Configuration for the Chrome Extension
 const CONFIG = {
   // NOTE: 사지방 dev env is based on vscode tunnel
-  BACKEND_URL: 'https://vnw20xbg-8080.asse.devtunnels.ms/',
+  BACKEND_URL: 'https://vnw20xbg-8080.asse.devtunnels.ms',
   API_ENDPOINTS: {
     CONVERT: '/convert',
     HEALTH: '/health'
