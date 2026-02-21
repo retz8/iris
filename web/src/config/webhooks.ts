3e8e2db0d89a372cbeeac0b2a9ff2989b2bd51d7
retz8 <ekkicb71@gmail.com>
2026-02-21 14:24:48 +0900

    fix: move webhook url to env

--- web/src/config/webhooks.ts
+++ web/src/config/webhooks.ts
@@ -1,4 +1,5 @@
 export const WEBHOOK_BASE =
   import.meta.env.VITE_APP_ENV === 'prod'
-    ? 'https://retz8.app.n8n.cloud/webhook'
-    : 'https://retz8.app.n8n.cloud/webhook-test';
+    ? import.meta.env.VITE_WEBHOOK_BASE_PROD
+    : import.meta.env.VITE_WEBHOOK_BASE_DEV;
+`
